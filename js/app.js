13ed6344ecd06d2253a351dc7321890dd3f3ebd7
BalJuganas <115352563+BalJuganas@users.noreply.github.com>
2022-12-17 22:33:32 +0800

    Delete app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-    let lefty= document.getElementById('left-pan');
-    let righty= document.getElementById('right-pan');
-
-    window.addEventListener('scroll', function(){
-        let value = window.scrollY;
-
-        lefty.style.left = value * 2 + 'px';
-    })
